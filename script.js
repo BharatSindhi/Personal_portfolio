7dfef89d46b3dff2a7f0cc676e8a267991eb53a4
bharat <sindhibharat567@gmail.com>
2024-03-11 09:47:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+let tablinks= document.getElementsByClassName("tab-link");
+let tabcontents= document.getElementsByClassName("tab-content");
+
+function opentab(tabname){
+ for(tablink of  tablinks){
+     tablink.classList.remove("active-link")
+ }
+ for(tabcontent of  tabcontents){
+     tabcontent.classList.remove("active-tab")
+ }
+ event.currentTarget.classList.add("active-link");
+ document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
+var sidemenu =document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right="0";
+}
+function closemenu(){
+    sidemenu.style.right="-200px";
+}
+
+function tab(){
+    location.href="#contact";
+    
+}
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxNUUmZy0sx80pCw0LWkF3_T7tJdxoKX2oD-F3OT_6ogqv3kiYjLENx30enJrSYt7Kggw/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg");
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="message send sucessfuly";
+        setTimeout(function(){
+            msg.innerHTML=""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+
+   
